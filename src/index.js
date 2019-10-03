e29cc980126e7e5ff1c97ecd57575a9252c6a9df
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-10-03 20:17:57 +0300

    feat: add temp array

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ module.exports = function multiply(first, second) {
   let one = first.split('').reverse();  // split and reverse first and second numbers
   let two = second.split('').reverse(); // into two arrays
 
+  let temp = [];                        // initializing temporary array for multiplying
+                                        // each number in arrays
+
   
 }
