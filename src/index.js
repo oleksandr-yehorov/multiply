a1d06d584610a18f424db8d385d54eb2ebf6dc72
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-10-03 20:17:07 +0300

    init: split and reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function multiply(first, second) {
   // your solution
+
+  let one = first.split('').reverse();  // split and reverse first and second numbers
+  let two = second.split('').reverse(); // into two arrays
+
+  
 }
