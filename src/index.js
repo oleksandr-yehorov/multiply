53663c16252731b53bb5cbed8403780981258a39
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-10-03 22:01:30 +0300

    feat: ашÐtemp array

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 module.exports = function multiply(first, second) {
-  // your solution
-
-  let one = first.split('').reverse();  // split and reverse first and second numbers
-  let two = second.split('').reverse(); // into two arrays
-
-  let temp = [];                        // initializing temporary array for multiplying
-                                        // each number in arrays
-
   
+  // split and reverse first and second numbers into two arrays
+
+  let one = first.split('').reverse();
+  let two = second.split('').reverse();
+
+  // initializing temporary array for multiplying each number in arrays
+
+  let temp = [];
+
+  // multiply each element and write it in temp
+  // if there is a record in temp - we add a number, if not, then we overwrite
+
+  for (var i = 0; i < one.length; i++) {
+    for (var j = 0; j < two.length; j++) {
+      var multi = one[i] * two[j];
+        if (temp[i + j]) {
+          temp[i + j] = temp[i + j] + multi
+        } else {
+          temp[i + j] = multi;
+        };
+    };
+  };
+ 
 }
